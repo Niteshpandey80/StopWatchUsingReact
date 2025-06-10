d7cc49c23076b436074c173b13abc981ab77efbb
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2025-06-10 23:30:36 +0530

    new change

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,41 @@
-  import React, { useRef, useState } from 'react'
+import React, { useRef, useState } from 'react';
 
-  const App = () => {
-    const [time , setTime ] = useState(0);
-    const stopwatchRef = useRef(0)
-    const intervalRef = useRef(null);
-    const [now , setNow] = useState(null)
+const App = () => {
+  const [time, setTime] = useState(0);
+  const stopwatchRef = useRef(0);
+  const intervalRef = useRef(null);
+  const [now, setNow] = useState(null);
 
-    const handleStart=()=>{
-        setTime(Date.now());
-        setNow(Date.now());
-        stopwatchRef.current = new Date().getTime(); 
-        intervalRef.current = setInterval(()=>{
-          setTime(new Date().getTime()-stopwatchRef.current);
-        },10) 
-    }
-    const handlePause=()=>{
-      clearInterval(intervalRef.current);
-    }
-    let timePassed = (time - null) / 1000;
-    const handleReset=()=>{
+  const handleStart = () => {
+    setNow(Date.now());
+    stopwatchRef.current = new Date().getTime(); 
+    intervalRef.current = setInterval(() => {
+      setTime(new Date().getTime() - stopwatchRef.current);
+    }, 10);
+  };
 
-    }
-    const formatTime =()=>{
-       const ms  = Math.floor((time % 1000)/10 );    
-    }
-    return (
-      <div className='stopwatch'>
-        <span className='time'>{timePassed.toFixed(3)}</span>
-        <div>
-          <button  onClick={handleStart}>Start</button>
-          <button onClick={handlePause}>Pause</button>
-          <button onClick={handleReset}>Reset</button>
-        </div>
-        
+  const handlePause = () => {
+    clearInterval(intervalRef.current);
+  };
+
+  const handleReset = () => {
+    clearInterval(intervalRef.current);
+    setTime(0);
+    setNow(null);
+  };
+
+  const timePassed = time / 1000;
+
+  return (
+    <div className='stopwatch'>
+      <span className='time'>{timePassed.toFixed(3)}</span>
+      <div>
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handlePause}>Pause</button>
+        <button onClick={handleReset}>Reset</button>
       </div>
-    )
-  }
+    </div>
+  );
+};
 
-  export default App
+export default App;
